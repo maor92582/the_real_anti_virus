4db7c8af05820bfaf6b0b2a86db689fe9a887bf7
maor92582 <maor.ifra@gmail.com>
2026-01-25 19:13:02 +0200

    מעבר ללינוקס

--- ContinerThreatMonitor.py
+++ ContinerThreatMonitor.py
@@ -5,6 +5,7 @@ import time
 def is_docker_installed():
     try:
         print(subprocess.run(["docker","--version"]))
+        return True
     except FileNotFoundError:
         print("downloading")
         file=requests.get("https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?utm_source=docker&utm_medium=webreferral&utm_campaign=dd-smartbutton&utm_location=module&_gl=1*2t2mu1*_gcl_au*MjA4NTg3NDEwNy4xNzYzMzAxMzg1*_ga*NTEwMTAxNzQ2LjE3NjMyOTU5MDI.*_ga_XJWPQMJYHQ*czE3NjMzMDEzODUkbzIkZzEkdDE3NjMzMDE0MzgkajckbDAkaDA.")
@@ -16,6 +17,7 @@ def is_docker_installed():
             subprocess.run(["download.exe","install","--quiet","--accept-license","--backend=wsl-2"])
             print("finish")
             subprocess.Popen(["C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"])
+            return True
             
 
 
@@ -36,11 +38,17 @@ def create_continer(timeout=45):
     d=False
     for i in range(timeout):
         time.sleep(1)
-        if(is_docker_installed):
+        if(is_docker_running()):
             d=True
             break
     if(d==True):
+        print("111")
         client=docker.from_env()
-        print(client.containers.run("mcr.microsoft.com/windows/servercore:ltsc2022",command="cmd.exe",name="win_test",detach=True))
+        continer=client.containers.run("debian:bookworm-slim","ls",name="linux_test",detach=True)
+        print("קונטיינר נוצר. ID:", continer)
+        print("לוגים ראשוניים:")
+        print(continer.logs().decode(errors="ignore"))
+        print("fff")
+
 #is_docker_installed()
 create_continer()
