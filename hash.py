b01a9cea66226b1a2633acca7231bb1f6bca0576
maor92582 <maor.ifra@gmail.com>
2026-01-25 19:12:57 +0200

    גרסה 1.1 מוריד את הקובץ העדכני כרגע

--- hash.py
+++ hash.py
@@ -1,27 +1,40 @@
 import hashlib
+import requests
+import zipfile
+import io
+
+def get_new_hashlist():
+    url="https://bazaar.abuse.ch/export/txt/sha256/full/"
+    x=requests.get(url)
+    print(x)
+    if(x.status_code==200):
+        with zipfile.ZipFile(io.BytesIO(x.content)) as z:
+            print(z.filelist)
+            return get_hash_set(z.extract("full_sha256.txt"))
+
+
 def file_hashing(filepath): #ממיר את הקובץ לsha256
     with open(filepath,'rb') as f:
         file_data = f.read()
         return hashlib.sha256(file_data).hexdigest()
-def get_hash_set(filepath):
+def get_hash_set(file):#מוצא hash חדש
     hashes=set()
-    with open(filepath,'r') as f:
+    with open(file,'r') as f:
         for line in f:
             line = line.strip()
             if not line or len(line) != 64:
                 continue  
             hashes.add(line)
     return hashes
-def check_if_sus(hashes,hash):
+def check_if_sus(hashes,hash):#משווא בין המאגר לhash
     if hash in hashes:
         return True
     return False
-filepath="readme.txt"
-file_hash  = file_hashing(filepath)
-print(file_hash )
-hashset=get_hash_set("data\\full_sha256.txt")
-print(check_if_sus(hashset,file_hash))
-          
-
+def run_check(filepath):
+    file_hash=file_hashing(filepath)
+    hashset  = get_new_hashlist()
+    print(check_if_sus(hashset,file_hash))
+            
+run_check("readme.txt")
 
 
